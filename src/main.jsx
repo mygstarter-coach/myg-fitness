0117262d3811fbb8f9a5f6aa5962954d753ba75f
mygstarter-coach <mygstarter@gmail.com>
2026-05-11 18:08:56 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import MYGFitness from "./myg-fitness.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
